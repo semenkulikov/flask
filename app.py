47aecf83e84f24456352847731cb682793dc38da
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-09-05 12:32:10 +0300

    Add logging to project

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 from models import init_db
+from logger import my_handler, stream_handler
+import logging
 app = Flask(__name__)
+
+app_logger = logging.getLogger("app_logger")
+app_logger.setLevel(logging.INFO)
+app_logger.addHandler(my_handler)
+app_logger.addHandler(stream_handler)
 
 
 @app.route('/')
@@ -10,4 +17,5 @@ def index():
 
 if __name__ == '__main__':
     init_db()  # Initialize the database before running the app.
+    app_logger.info("Starting the Flask application...")  # Log the start of the app
     app.run(debug=True)
